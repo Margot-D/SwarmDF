c966ef370a77a1b89040e8479919f72dd65e90a6
Margot-D <margot.decotte@gmail.com>
2026-05-11 18:30:03 +0200

    Created configuration class for user inputs

---
+++ src/swarmdf/gui/config.py
@@ -0,0 +1,52 @@
+#TODO add small docu here 
+
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class SwarmDFConfig:
+
+    # satellite
+    sat_id: str
+
+    # time interval
+    start_time: datetime
+    end_time: datetime
+    timestep: float
+
+    # datasets
+    datasets2download: list
+
+    # conductance
+    conductance_method: str
+    conductance_params: dict
+
+    # grid
+    grid_params: dict
+
+    # lompe
+    run_lompe_flag: bool
+
+    # regularization
+    l1: float
+    l2: float
+
+    # plotting
+    speed: int
+    figw: float
+    figh: float
+
+    # plot options
+    mag: bool
+    show_data: bool
+
+    # validation
+    run_validation_flag: bool
+    timeoff: int
+    snapshot: int
+
+    # demo mode
+    demo: bool
+
+    # python script 
+    generate_script_flag: bool
