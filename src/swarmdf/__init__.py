aefd02f1ee47c10175557a5ec0191723ca7253cb
Margot-D <margot.decotte@gmail.com>
2026-05-15 19:08:01 +0200

    commented lompeosse import in init

--- src/swarmdf/__init__.py
+++ src/swarmdf/__init__.py
@@ -2,7 +2,7 @@ from swarmdf.core.data_manager import DataManager
 from swarmdf.core.lompe_input import LompeInput
 from swarmdf.core.lompe_analysis import run_lompe, plot_lompe_output
 from swarmdf.core.conductance import compute_conductances
-from swarmdf.core.lompeosse_analysis import run_lompeOSSE, plot_lompeOSSE_output
+# from swarmdf.core.lompeosse_analysis import run_lompeOSSE, plot_lompeOSSE_output
 from swarmdf.core.code_generator import generate_python_code
 
 #TODO add stuff here
