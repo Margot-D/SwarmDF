f990153bc87ee307add57a402e210b335811edaf
Margot-D <margot.decotte@gmail.com>
2026-05-15 19:07:32 +0200

    removed build_config_dict

--- src/swarmdf/__init__.py
+++ src/swarmdf/__init__.py
@@ -3,6 +3,8 @@ from swarmdf.core.lompe_input import LompeInput
 from swarmdf.core.lompe_analysis import run_lompe, plot_lompe_output
 from swarmdf.core.conductance import compute_conductances
 from swarmdf.core.lompeosse_analysis import run_lompeOSSE, plot_lompeOSSE_output
-from swarmdf.core.code_generator import build_config_dict, generate_python_code
+from swarmdf.core.code_generator import generate_python_code
 
-__all__ = ["DataManager", "LompeInput", "run_lompe", "plot_lompe_output", "compute_conductances", "run_lompeOSSE", "plot_lompeOSSE_output", "build_config_dict", "generate_python_code"]
+#TODO add stuff here
+
+__all__ = ["DataManager", "LompeInput", "run_lompe", "plot_lompe_output", "compute_conductances", "run_lompeOSSE", "plot_lompeOSSE_output", "generate_python_code"]
