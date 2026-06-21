5999b08e35571949c4b2e26d62e0d79cb0534281
Margot-D <margot.decotte@gmail.com>
2026-06-22 00:19:55 +0200

    Separated lompeinput class into 2 smaller classes

--- src/swarmdf/__init__.py
+++ src/swarmdf/__init__.py
@@ -1,8 +1,8 @@
 from swarmdf.core.data_manager import DataManager
-from swarmdf.core.lompe_input import LompeInput
+from swarmdf.core.lompe_input import LompeInputBuilder, LompeInputPlotter
 from swarmdf.core.lompe_analysis import run_lompe, plot_lompe_output
 from swarmdf.core.conductance import compute_conductances
 # from swarmdf.core.lompeosse_analysis import run_lompeOSSE, plot_lompeOSSE_output
-from swarmdf.core.code_generator import generate_python_code
+from swarmdf.core.script_generator import generate_python_code
 
 # __all__ = ["DataManager", "LompeInput", "run_lompe", "plot_lompe_output", "compute_conductances", "run_lompeOSSE", "plot_lompeOSSE_output", "generate_python_code"]
