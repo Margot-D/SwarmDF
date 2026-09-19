00789e49ddb41ddddb62098d74af6d4ca3458663
Margot-D <margot.decotte@gmail.com>
2026-09-19 17:49:12 +0200

    Added lompeOSSE back to init

--- src/swarmdf/__init__.py
+++ src/swarmdf/__init__.py
@@ -2,7 +2,7 @@ from swarmdf.core.data_manager import DataManager
 from swarmdf.core.lompe_input import LompeInputBuilder, LompeInputPlotter
 from swarmdf.core.lompe_analysis import run_lompe, plot_lompe_output
 from swarmdf.core.conductance import compute_conductances
-# from swarmdf.core.lompeosse_analysis import run_lompeOSSE, plot_lompeOSSE_output
+from swarmdf.core.lompeosse_analysis import run_lompeOSSE, plot_lompeOSSE_output
 from swarmdf.core.script_generator import generate_python_code
 
 # __all__ = ["DataManager", "LompeInput", "run_lompe", "plot_lompe_output", "compute_conductances", "run_lompeOSSE", "plot_lompeOSSE_output", "generate_python_code"]
